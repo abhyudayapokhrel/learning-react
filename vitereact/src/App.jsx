c7a586360727a1459b90dd47e1f1b686d9753c23
abhyudayapokhrel <abhyudayapokhrel23@gmail.com>
2025-04-05 14:57:22 +0545

    a

--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <p>This is my project | {username}</p> 
     {/* username is evaluated expression of javascript so we cannnot perform any evaluation in return do it before and pass it*/}
     <p>Something in the way</p>
+    <p>Tears in Heaven</p>
     <button>Click Me!</button>
     </>
   )
