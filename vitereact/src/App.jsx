bf5be4032545bac9529eb9ea148e0cf571555fd5
abhyudayapokhrel <abhyudayapokhrel23@gmail.com>
2025-04-07 21:33:34 +0545

    a

--- vitereact/src/App.jsx
+++ vitereact/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     {/* username is evaluated expression of javascript so we cannnot perform any evaluation in return do it before and pass it*/}
     <p>Something in the way</p>
     <p>Tears in Heaven</p>
+    <p>Hey Jude</p>
     <button>Click Me!</button>
     <a href="https://google.com">Click Me</a>
     </>
