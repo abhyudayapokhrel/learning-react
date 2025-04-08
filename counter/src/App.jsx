387610520650657a296ea6db589a52e55dc8331e
abhyudayapokhrel <abhyudayapokhrel23@gmail.com>
2025-04-08 22:51:49 +0545

    basic

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,14 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- 
+
+  let [counter, setCounter] = useState(15)
+
+//  let counter = 15;
+
+ const addValue = () =>{
+    //counter++;
+    // counter value increases but is not shown in the DOM
+    setCounter(counter+1)
+ }
+
+ const removeValue = () =>{
+  setCounter(counter-1)
+ }
+
 
   return (
     <>
       <h1>Counter App</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br /> <br />
+      <button onClick={removeValue}>Decrease Value</button>
     </>
   )
 }
