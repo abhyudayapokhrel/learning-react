aabf01946c882f92aaafef3038d8eb93ed4dc6eb
abhyudayapokhrel <abhyudayapokhrel23@gmail.com>
2025-04-22 19:11:13 +0545

    bg

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -12,6 +12,9 @@ function App() {
             <button onClick={()=> setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "red"}}>Red</button>
             <button onClick={()=> setColor("green")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "green"}}>Green</button>
             <button onClick={()=> setColor("blue ")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "blue"}}>Blue</button>
+            <button onClick={()=> setColor("purple ")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "purple"}}>purple</button>
+            <button onClick={()=> setColor("orange ")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "orange"}}>orange</button>
+            <button onClick={()=> setColor("black ")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "black"}}>black</button>
 
           </div>
 
